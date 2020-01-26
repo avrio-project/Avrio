fa4dcf760a512260766423a53f225c9fc6eccd30
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-01-26 17:03:34 +0000

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -64,7 +64,7 @@ namespace CryptoNote
         const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE            =                                       600;
 
         const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT                  =                                         4;
-        const uint64_t MINIMUM_FEE                                       =                             UINT64_C(250); 0.25
+        const uint64_t MINIMUM_FEE                                       =                             UINT64_C(250); //0.25
 
         /* This section defines our minimum and maximum mixin counts required for transactions */
         const uint64_t MINIMUM_MIXIN_V1                        =                                      0;
