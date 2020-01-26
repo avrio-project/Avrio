4f792a772304fcaa85bd5dffd7b225f7f91c533d
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-01-26 18:27:01 +0000

    Update mixins

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -77,14 +77,14 @@ namespace CryptoNote
         const uint64_t MAXIMUM_MIXIN_V3                        =                                      2;
 
         /* The heights to activate the mixin limits at */
-        const uint32_t MIXIN_LIMITS_V1_HEIGHT                  =                                 10'000;
+        const uint32_t MIXIN_LIMITS_V1_HEIGHT                  =                                 5;
         const uint32_t MIXIN_LIMITS_V2_HEIGHT                  =                                200'000;
         const uint32_t MIXIN_LIMITS_V3_HEIGHT                  =                                500'000;
 
-        const uint64_t DEFAULT_MIXIN_V0                        =                                      0;
+        const uint64_t DEFAULT_MIXIN_V0                        =                                      1;
         const uint64_t DEFAULT_MIXIN_V1                        =                                      1;
         const uint64_t DEFAULT_MIXIN_V2                        =                                      3;
-        const uint64_t DEFAULT_MIXIN_V3                        =                       MAXIMUM_MIXIN_V3;
+        const uint64_t DEFAULT_MIXIN_V3                        =                                      2;
 
         const uint64_t DEFAULT_DUST_THRESHOLD                  =                          UINT64_C(100);
         const uint64_t DEFAULT_DUST_THRESHOLD_V2               =                            UINT64_C(0);
